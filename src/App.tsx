e4579659d1f71bc1e20ff04fc4dde9d6cf204a39
duongbao0803 <tonbao0803@gmail.com>
2023-12-28 11:16:58 +0700

    SETUP tailwind, typescript environment

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="grid grid-cols-3">
+        <div className="text-[#fff123]">Hello</div>
+        <div>Hello1</div>
+        <div>Hello2</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
