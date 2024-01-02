ec7e022aa13f47f14d1c84f896a1e621de991203
duongbao0803 <tonbao0803@gmail.com>
2024-01-02 11:25:39 +0700

    UPDATE - Change port

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
+import React from "react";
 import "./App.css";
 
 function App() {
+  const text = [
+    { name: "helo" },
+    {
+      name: "bao",
+      age: "20",
+    },
+    {
+      name: "bao",
+      age: "20",
+    },
+    {
+      name: "bao",
+      age: "20",
+    },
+    {
+      name: "bao",
+      age: "20",
+    },
+    {
+      name: "bao",
+      age: "20",
+    },
+  ];
   return (
     <>
-      <div className="grid grid-cols-3">
-        <div className="text-[#fff123]">Hello</div>
-        <div>Hello1</div>
-        <div>Hello2</div>
+      <div className="grid grid-cols-3 text-center">
+        {text.map((tester, index) => (
+          <React.Fragment key={index}>
+            <div>{index}</div>
+            <div key={`name_${index}`}>{tester.name}</div>
+          </React.Fragment>
+        ))}
       </div>
     </>
   );
